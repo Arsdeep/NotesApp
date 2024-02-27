38f0c0c1d5d146f018ebe26b2a15e0dac985cae8
Wolfey <61946367+Arsdeep@users.noreply.github.com>
2024-02-27 12:07:26 +0530

    Update Card.js
    implemented code for Random Images

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function Card() {
+  const randNo = Math.floor(Math.random()*100)+1;
+  const imageUrl = `https://source.unsplash.com/random/300x300?sig=${randNo}`;
+
   return (
     <div className="card" style={{width:18 + 'rem', margin:1+'rem'}}>
-        <img src="https://source.unsplash.com/random/300x300?sig=4" className="card-img-top" alt="..."/>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
